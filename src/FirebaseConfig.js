6d154cd7f7f3d48e53a20832575eb1f818f7e1dd
Somsubhra Das <das.somsubhra1@gmail.com>
2018-11-03 15:04:09 +0530

    Delete FirebaseConfig.js

--- src/FirebaseConfig.js
+++
@@ -1,9 +0,0 @@
-var config = {
-  apiKey: "AIzaSyAS_vAUIqrXj12xzcHXnX4o9HspegqnVVs",
-  authDomain: "fir-login-5fce5.firebaseapp.com",
-  databaseURL: "https://fir-login-5fce5.firebaseio.com",
-  projectId: "fir-login-5fce5",
-  storageBucket: "fir-login-5fce5.appspot.com",
-  messagingSenderId: "821377824687"
-};
-export default config;
